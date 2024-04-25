d573e9422a3ff5e12c129b634cbf3d26e7fd59ab
Lobinda <lobinda123@gmail.com>
2024-04-25 09:07:35 +0800

    update

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var cors = require('cors')
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+const postRouter = require('./routes/posts');
+const mongoose = require('mongoose');
 var app = express();
 
+
+const dotenv = require('dotenv');
+
+dotenv.config({path: './config.env'});
+
+const constr = process.env.DATABASE.replace('<password>',process.env.DATABASE_PASSWORD);
+
+//
+mongoose.connect(constr)
+    .then(res => console.log("連線資料成功"));
+
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/api/posts', postRouter);
 
 module.exports = app;
