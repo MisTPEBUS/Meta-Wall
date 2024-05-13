6ace3525b73b16d3c5e8a57297841767f22f9d06
Lobinda <lobinda123@gmail.com>
2024-05-13 09:44:05 +0800

    新增route users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//註冊
+router.post('/sign_up', function(req, res, next) {
+  res.send('respond sign_up');
 });
+//登入
+router.post('/sign_in', function(req, res, next) {
+  res.send('respond sign_in');
+});
+//登出
+router.post('/sign_out', function(req, res, next) {
+  res.send('respond sign_out');
+});
+//驗證
+router.get('/checkout', function(req, res, next) {
+  res.send('respond checkout');
+});
+
+
 
 module.exports = router;
