ff3d4f26b38ad7c70fa5d21fc2d7ab8441660900
Lobinda <lobinda123@gmail.com>
2024-05-19 15:57:05 +0800

    新新增middleware+res

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const Todo = require('../models/todos');
+const handleErrorAsync = require('../service/handleErrorAsync.js');
+const appError = require('../service/appError.js');
+const { isAuth, blackListCheck, generateSendJWT } = require('../service/auth');
 
 //讀取清單
-router.get('/', function (req, res, next) {
-
+router.get('/', isAuth, async function (req, res, next) {
     /*
         #swagger.tags =  ['待辦事項']
         #swagger.path = '/todos'
@@ -38,10 +41,32 @@ router.get('/', function (req, res, next) {
             }
             } 
         */
-    res.send('respond sign_up');
+
+    /*  const post = await Todo.find(q).populate({
+         path: 'user',
+         select: 'name photo '
+       }).populate({
+         path: 'comments',
+         select: 'comment user'
+       }).sort(timeSort);
+     // res.send('respond with a resource');
+     res.status(200).json({
+       post
+     })       */
+
+
+    const todos = await Todo.find({ user: req.user._id });
+
+    console.log(todos);
+    res.status(200).json({
+        "status": true,
+        "message": "",
+        todos
+    })
 });
 //新增代辦
-router.post('/', function (req, res, next) {
+router.post('/', isAuth, handleErrorAsync(async (req, res, next) => {
+
 
     /*
        #swagger.tags =  ['待辦事項']
@@ -74,10 +99,23 @@ router.post('/', function (req, res, next) {
              } 
          */
 
-    res.send('respond sign_in');
-});
+    const { content } = req.body;
+    if (!content) {
+        return next(appError(400, "你沒有填寫 content 資料", next))
+    }
+
+    console.log(req.user._id.toHexString())
+    const newTodo = await Todo.create({
+        user: req.user._id.toHexString(),
+        content
+    });
+    res.status(200).json({
+        "status": true,
+        "message": "新增成功!",
+    })
+}));
 //修改代辦
-router.put('/{id}', function (req, res, next) {
+router.put('/:id', isAuth, handleErrorAsync(async (req, res, next) => {
 
     /*
        #swagger.tags =  ['待辦事項']
@@ -104,7 +142,7 @@ router.put('/{id}', function (req, res, next) {
          #swagger.responses[200] = { 
              schema: {
                  "status": true,
-                 "message": "新增成功!",
+                 "message": "修改成功!",
              }
          } 
          #swagger.responses[400] = { 
@@ -114,10 +152,26 @@ router.put('/{id}', function (req, res, next) {
              }
              } 
          */
-    res.send('respond sign_out');
-});
+
+    if (!req.body.content) {
+        return next(appError(400, "你沒有填寫 content 資料", next))
+    }
+    const updatedTodo = await Todo.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+            user: req.user._id,
+            content: req.body.content
+        },
+        { new: true }  // 返回更新后的文档
+    );
+    res.status(200).json({
+        "status": true,
+        "message": "修改成功!",
+        todo: updatedTodo
+    })
+}));
 //刪除代辦
-router.delete('/{id}', function (req, res, next) {
+router.delete('/:id', isAuth, handleErrorAsync(async (req, res, next) => {
     /*
        #swagger.tags =  ['待辦事項']
        #swagger.path = '/todos/{id}'
@@ -148,10 +202,19 @@ router.delete('/{id}', function (req, res, next) {
              } 
          */
 
-    res.send('respond checkout');
-});
+    
+    const deleteTodo = await Todo.findByIdAndDelete(
+        { _id: req.params.id },
+        { new: true }  // 返回更新后的文档
+    );
+    res.status(200).json({
+        "status": true,
+        "message": "刪除成功!",
+        todo: deleteTodo
+    })
+}));
 //設定待辦完成
-router.patch('/{id}/toggle', function (req, res, next) {
+router.patch('/:id/toggle', isAuth, handleErrorAsync(async (req, res, next) => {
     /*
        #swagger.tags =  ['待辦事項']
        #swagger.path = '/todos/{id}'
@@ -159,14 +222,9 @@ router.patch('/{id}/toggle', function (req, res, next) {
        #swagger.description = '設定待辦完成'
        #swagger.summary = '設定待辦完成'
        #swagger.produces = ["application/json"]
-               #swagger.security = [{'BearerAuth': []}]
+       #swagger.security = [{'BearerAuth': []}]
    */
     /*
-         #swagger.parameters['id'] = {
-             in: 'body',
-             required: true,
-            description:"待辦ID"
-         }
          #swagger.responses[200] = { 
              schema: {
                  "status": true,
@@ -181,7 +239,21 @@ router.patch('/{id}/toggle', function (req, res, next) {
              } 
          */
 
-    res.send('respond checkout');
-});
+    let updatedTodo = await Todo.findById({ _id: req.params.id })
+    updatedTodo.status = !updatedTodo.status
+    updatedTodo = await Todo.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+            user: req.user._id,
+            status: updatedTodo.status
+        },
+        { new: true }  // 返回更新后的文档
+    );
+    res.status(200).json({
+        "status": true,
+        "message": "修改成功!",
+        todo: updatedTodo
+    })
+}));
 
 module.exports = router;
