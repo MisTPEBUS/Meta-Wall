049416c84a834227562eefe3aafec5a145dca9a7
Lobinda <lobinda123@gmail.com>
2024-05-19 13:11:35 +0800

    加入自訂錯誤處裡

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/todos', todosRouter);
-app.use('/api/', usersRouter);
+app.use('/api', usersRouter);
 
 //route 404 
 app.use(function (req, res, next) {
@@ -47,6 +47,49 @@ app.use(function (req, res, next) {
       message: "查無此路由，請確認API格式!",
     });
   });
+
+  // express 錯誤處理
+// 自己設定的 err 錯誤 
+const resErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: false,
+      message: err.message
+    });
+  } else {
+    // log 紀錄
+    console.error('出現重大錯誤', err);
+    // 送出罐頭預設訊息
+    res.status(500).json({
+      status: 'error',
+      message: '系統錯誤，請恰系統管理員'
+    });
+  }
+};
+// 開發環境錯誤
+const resErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    message: err.message,
+    error: err,
+    stack: err.stack
+  });
+};
+// 錯誤處理
+app.use(function(err, req, res, next) {
+  // dev
+  err.statusCode = err.statusCode || 500;
+  if (process.env.NODE_ENV === 'dev') {
+    return resErrorDev(err, res);
+  } 
+  // production
+  if (err.name === 'ValidationError'){
+    console.error(err)
+    err.message = "資料欄位未填寫正確，請重新輸入！"
+    err.isOperational = true;
+    return resErrorProd(err, res)
+  }
+  resErrorProd(err, res)
+});
 
   //  catch none error 
 process.on('unhandledRejection', (reason, promise) => {
